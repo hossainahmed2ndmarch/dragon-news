d98ef5ee4034491c9b537c8c52733be0bc3d1465
hossainahmed2ndmarch <hossainahmed2ndmarch@gmail.co>
2024-11-13 22:50:15 +0600

    Initial project set up complete

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="py-2">
+      <h1 className="headernews text-center">The Dragon News</h1>
+      <p className="text-lg text-[#706F6F] text-center">Journalism Without Fear or Favour</p>
+    </div>
+  );
+};
+
+export default Header;
